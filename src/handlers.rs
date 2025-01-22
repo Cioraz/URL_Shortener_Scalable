485ae643639b27a1f898ac7f31ae6ca59ac526eb
Cioraz <sunilthunga.221cs252@nitk.edu.in>
2025-01-22 13:13:23 +0530

    First Draft of working API

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,10 +1,8 @@
 use crate::db;
+use rand::seq::IteratorRandom;
+use ring::digest::{Context, SHA256};
 use std::collections::HashMap;
-use warp::{
-    http::StatusCode,
-    reply::{self, Reply},
-    Filter,
-};
+use warp::{http::StatusCode, reply::Reply, Filter};
 
 pub fn with_db(
     database: db::Database,
@@ -18,32 +16,58 @@ pub async fn handle_generate_url(
     database: db::Database,
     api_key: String,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    if key
-        != std::env::var("API_KEY")
-            .expect("API_KEY wasnt provided!")
-            .to_string()
-    {
+    if key != api_key {
         return Ok(warp::reply::with_status(
-            "UNAUTHORIZED",
+            warp::reply::json(&serde_json::json!({
+                "error": "UNAUTHORIZED"
+            })),
             StatusCode::UNAUTHORIZED,
         ));
     }
     let long_url = body["long_url"].as_str().unwrap_or("");
     if long_url.is_empty() {
         return Ok(warp::reply::with_status(
-            "INVALID URL",
+            warp::reply::json(&serde_json::json!({
+                "error": "INVALID URL"
+            })),
             StatusCode::BAD_REQUEST,
         ));
     }
     let short_url = generate_short_url(long_url);
+    println!("Short URL: {}, Long URL: {}", &short_url, long_url);
     database
         .lock()
         .unwrap()
         .insert(short_url.clone(), long_url.to_string());
-    let response = warp::reply::json(&serde_json::json!({
-        "short_url":short_url
-    }));
+    let response_body = serde_json::json!({
+        "short_url": short_url
+    });
+
+    // Serialize the JSON response body to a string for printing
+    if let Ok(response_string) = serde_json::to_string(&response_body) {
+        println!("Generated response: {}", response_string);
+    }
+
+    let response = warp::reply::json(&response_body);
     Ok(warp::reply::with_status(response, StatusCode::OK))
+}
+
+fn generate_short_url(long_url: &str) -> String {
+    let mut context = Context::new(&SHA256);
+    context.update(long_url.as_bytes());
+    let hash = context.finish();
+    // So that it fits in u128 for later base62 encoding
+    let truncated_hash = u128::from_le_bytes(hash.as_ref()[0..16].try_into().unwrap());
+    let base62_encoded = base62::encode(truncated_hash);
+
+    // Taking 7 characters from base62_encoded for random short URL id
+    let mut rng = rand::thread_rng();
+    let short_url_id: String = base62_encoded
+        .chars()
+        .choose_multiple(&mut rng, 7)
+        .into_iter()
+        .collect();
+    short_url_id
 }
 
 pub async fn handle_redirect_url(
@@ -54,14 +78,24 @@ pub async fn handle_redirect_url(
     let db_lock = database.lock().unwrap();
 
     if let Some(long_url) = db_lock.get(&short_url) {
-        if let Ok(uri) = warp::http::Uri::try_from(long_url.as_str()) {
-            return Ok(warp::redirect::permanent(uri).into_response());
+        if let Ok(_uri) = warp::http::Uri::try_from(long_url.as_str()) {
+            return Ok(warp::reply::json(&serde_json::json!({
+                "status": "success",
+                "redirect_to": long_url
+            }))
+            .into_response());
         }
 
-        return Ok(
-            warp::reply::with_status("Invalid URL", StatusCode::BAD_REQUEST).into_response(),
-        );
+        return Ok(warp::reply::json(&serde_json::json!({
+            "status": "error",
+            "message": "Invalid URL format"
+        }))
+        .into_response());
     }
 
-    Ok(reply::with_status("Not Found", StatusCode::NOT_FOUND).into_response())
+    Ok(warp::reply::json(&serde_json::json!({
+        "status": "error",
+        "message": "Short URL not found"
+    }))
+    .into_response())
 }
--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,30 @@ async fn main() {
     dotenv().ok();
     let api_key = std::env::var("API_KEY").expect("API_KEY Not initialised!");
     let database = db::init_db();
+    let db1 = database.clone();
 
     let generate_url = warp::path("generate_url")
         .and(warp::post())
         .and(warp::header::<String>("API-Key"))
+        .and(warp::path::end())
         .and(warp::body::json())
-        .and(handlers::with_db(database))
+        .and(handlers::with_db(db1))
         .and_then(move |key, body: serde_json::Value, db| {
             let api_key = api_key.to_string();
             handlers::handle_generate_url(key, body, db, api_key)
         });
 
+    let db2 = database.clone();
     let redirect_url = warp::path("redirect_url")
         .and(warp::get())
         .and(warp::query::<HashMap<String, String>>())
-        .and(with_db(database))
+        .and(with_db(db2))
         .and_then(handlers::handle_redirect_url);
 
     let routes = generate_url.or(redirect_url);
-    let host = std::env::var("HOST_ADDR").unwrap_or_else(|_| "127.0.0.1".to_string());
-    let host_port = std::env::var("HOST_PORT")
-        .unwrap_or_else(|_| "3030".to_string())
+    let host = std::env::var("HOST_ADDR_1").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let host_port = std::env::var("HOST_PORT_1")
+        .unwrap_or_else(|_| "15555".to_string())
         .parse::<u16>()
         .expect("PORT must be a number!");
     let host_parts: Vec<u8> = host
