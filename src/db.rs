313aa529bc82bbea5d217ef2cb1bfc0b15d6f41a
Cioraz <sunilthunga.221cs252@nitk.edu.in>
2025-01-24 00:09:29 +0530

    Updated DB fields

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,14 @@
+use chrono::prelude::*;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-pub type Database = Arc<Mutex<HashMap<String, String>>>;
+pub type Database = Arc<Mutex<HashMap<String, Data>>>;
+
+pub struct Data {
+    pub creation_data: DateTime<Local>,
+    pub shortened_url: String,
+    pub long_url: String,
+    pub ttl: u8,
+}
 
 pub fn init_db() -> Database {
     Arc::new(Mutex::new(HashMap::new()))
--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,6 @@
 use crate::db;
+use chrono::prelude::*;
+use http::Uri;
 use rand::seq::IteratorRandom;
 use ring::digest::{Context, SHA256};
 use std::collections::HashMap;
@@ -35,11 +37,15 @@ pub async fn handle_generate_url(
     }
     let short_url = generate_short_url(long_url);
     println!("Short URL: {}, Long URL: {}", &short_url, long_url);
-    database
-        .lock()
-        .unwrap()
-        .insert(short_url.clone(), long_url.to_string());
+    let data = db::Data {
+        creation_data: Local::now(),
+        shortened_url: format!("{}/{}", "localhost", &short_url),
+        long_url: long_url.to_string(),
+        ttl: 30,
+    };
+    database.lock().unwrap().insert(short_url.clone(), data);
     let response_body = serde_json::json!({
+        "status": "success",
         "short_url": short_url
     });
 
@@ -52,7 +58,7 @@ pub async fn handle_generate_url(
     Ok(warp::reply::with_status(response, StatusCode::OK))
 }
 
-fn generate_short_url(long_url: &str) -> String {
+pub fn generate_short_url(long_url: &str) -> String {
     let mut context = Context::new(&SHA256);
     context.update(long_url.as_bytes());
     let hash = context.finish();
@@ -77,20 +83,34 @@ pub async fn handle_redirect_url(
     let short_url = params.get("short_url").cloned().unwrap_or_default();
     let db_lock = database.lock().unwrap();
 
-    if let Some(long_url) = db_lock.get(&short_url) {
-        if let Ok(_uri) = warp::http::Uri::try_from(long_url.as_str()) {
+    if let Some(data) = db_lock.get(&short_url) {
+        let now = Local::now();
+        let expiration_time = data.creation_data + chrono::Duration::seconds(data.ttl.into());
+
+        if now > expiration_time {
             return Ok(warp::reply::json(&serde_json::json!({
-                "status": "success",
-                "redirect_to": long_url
+                "status": "error",
+                "message": "Short URL has expired!"
             }))
             .into_response());
         }
 
-        return Ok(warp::reply::json(&serde_json::json!({
-            "status": "error",
-            "message": "Invalid URL format"
-        }))
-        .into_response());
+        match Uri::try_from(data.long_url.as_str()) {
+            Ok(_uri) => {
+                return Ok(warp::reply::json(&serde_json::json!({
+                    "status":"success",
+                    "redirect_to": data.long_url
+                }))
+                .into_response());
+            }
+            Err(_) => {
+                return Ok(warp::reply::json(&serde_json::json!({
+                    "status":"error",
+                    "message": "Invalid URL Format!"
+                }))
+                .into_response());
+            }
+        }
     }
 
     Ok(warp::reply::json(&serde_json::json!({
