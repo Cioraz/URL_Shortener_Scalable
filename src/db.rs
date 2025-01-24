71e89e99499f28030d010d8383d45187978b69f9
konatham naga mukesh <konathamnagamukesh2005@gmail.com>
2025-01-24 23:55:22 +0530

    removed example code in db.rs

--- src/db.rs
+++ src/db.rs
@@ -62,93 +62,93 @@ pub fn delete_data(database: Database, key: &str) -> RedisResult<()> {
 }
 
 /// Example usage of the database in your `handle_generate_url` function
-pub async fn handle_generate_url(
-    key: String,
-    body: serde_json::Value,
-    database: Database,
-    api_key: String,
-) -> Result<impl warp::Reply, warp::Rejection> {
-    if key != api_key {
-        return Ok(warp::reply::with_status(
-            warp::reply::json(&serde_json::json!({
-                "error": "UNAUTHORIZED"
-            })),
-            warp::http::StatusCode::UNAUTHORIZED,
-        ));
-    }
+// pub async fn handle_generate_url(
+//     key: String,
+//     body: serde_json::Value,
+//     database: Database,
+//     api_key: String,
+// ) -> Result<impl warp::Reply, warp::Rejection> {
+//     if key != api_key {
+//         return Ok(warp::reply::with_status(
+//             warp::reply::json(&serde_json::json!({
+//                 "error": "UNAUTHORIZED"
+//             })),
+//             warp::http::StatusCode::UNAUTHORIZED,
+//         ));
+//     }
 
-    let long_url = body["long_url"].as_str().unwrap_or("");
-    if long_url.is_empty() {
-        return Ok(warp::reply::with_status(
-            warp::reply::json(&serde_json::json!({
-                "error": "INVALID URL"
-            })),
-            warp::http::StatusCode::BAD_REQUEST,
-        ));
-    }
+//     let long_url = body["long_url"].as_str().unwrap_or("");
+//     if long_url.is_empty() {
+//         return Ok(warp::reply::with_status(
+//             warp::reply::json(&serde_json::json!({
+//                 "error": "INVALID URL"
+//             })),
+//             warp::http::StatusCode::BAD_REQUEST,
+//         ));
+//     }
 
-    let short_url = generate_short_url(long_url);
-    println!("Short URL: {}, Long URL: {}", &short_url, long_url);
+//     let short_url = generate_short_url(long_url);
+//     println!("Short URL: {}, Long URL: {}", &short_url, long_url);
 
-    let data = Data {
-        creation_data: chrono::Local::now().to_rfc3339(),
-        shortened_url: format!("{}/{}", "localhost", &short_url),
-        long_url: long_url.to_string(),
-        ttl: 30,
-    };
+//     let data = Data {
+//         creation_data: chrono::Local::now().to_rfc3339(),
+//         shortened_url: format!("{}/{}", "localhost", &short_url),
+//         long_url: long_url.to_string(),
+//         ttl: 30,
+//     };
 
-    // Store the data in Redis
-    if let Err(err) = store_data(database.clone(), short_url.clone(), data) {
-        eprintln!("Error storing data in Redis: {}", err);
-        return Ok(warp::reply::with_status(
-            warp::reply::json(&serde_json::json!({
-                "error": "INTERNAL SERVER ERROR"
-            })),
-            warp::http::StatusCode::INTERNAL_SERVER_ERROR,
-        ));
-    }
+//     // Store the data in Redis
+//     if let Err(err) = store_data(database.clone(), short_url.clone(), data) {
+//         eprintln!("Error storing data in Redis: {}", err);
+//         return Ok(warp::reply::with_status(
+//             warp::reply::json(&serde_json::json!({
+//                 "error": "INTERNAL SERVER ERROR"
+//             })),
+//             warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+//         ));
+//     }
 
-    let response_body = serde_json::json!({
-        "status": "success",
-        "short_url": short_url
-    });
+//     let response_body = serde_json::json!({
+//         "status": "success",
+//         "short_url": short_url
+//     });
 
-    if let Ok(response_string) = serde_json::to_string(&response_body) {
-        println!("Generated response: {}", response_string);
-    }
+//     if let Ok(response_string) = serde_json::to_string(&response_body) {
+//         println!("Generated response: {}", response_string);
+//     }
 
-    let response = warp::reply::json(&response_body);
-    Ok(warp::reply::with_status(response, warp::http::StatusCode::OK))
-}
+//     let response = warp::reply::json(&response_body);
+//     Ok(warp::reply::with_status(response, warp::http::StatusCode::OK))
+// }
 
 /// Example usage of the database in your `handle_redirect_url` function
-pub async fn handle_redirect_url(
-    params: HashMap<String, String>,
-    database: Database,
-) -> Result<impl warp::Reply, warp::Rejection> {
-    let short_url = params.get("short_url").cloned().unwrap_or_default();
+// pub async fn handle_redirect_url(
+//     params: HashMap<String, String>,
+//     database: Database,
+// ) -> Result<impl warp::Reply, warp::Rejection> {
+//     let short_url = params.get("short_url").cloned().unwrap_or_default();
 
-    if let Some(data) = retrieve_data(database.clone(), &short_url) {
-        let now = chrono::Local::now();
-        let expiration_time = chrono::DateTime::parse_from_rfc3339(&data.creation_data)
-            .unwrap()
-            + chrono::Duration::seconds(data.ttl.into());
+//     if let Some(data) = retrieve_data(database.clone(), &short_url) {
+//         let now = chrono::Local::now();
+//         let expiration_time = chrono::DateTime::parse_from_rfc3339(&data.creation_data)
+//             .unwrap()
+//             + chrono::Duration::seconds(data.ttl.into());
 
-        if now > expiration_time {
-            return Ok(warp::reply::json(&serde_json::json!({
-                "status": "error",
-                "message": "Short URL has expired!"
-            })));
-        }
+//         if now > expiration_time {
+//             return Ok(warp::reply::json(&serde_json::json!({
+//                 "status": "error",
+//                 "message": "Short URL has expired!"
+//             })));
+//         }
 
-        return Ok(warp::reply::json(&serde_json::json!({
-            "status": "success",
-            "redirect_to": data.long_url
-        })));
-    }
+//         return Ok(warp::reply::json(&serde_json::json!({
+//             "status": "success",
+//             "redirect_to": data.long_url
+//         })));
+//     }
 
-    Ok(warp::reply::json(&serde_json::json!({
-        "status": "error",
-        "message": "Short URL not found"
-    })))
-}
+//     Ok(warp::reply::json(&serde_json::json!({
+//         "status": "error",
+//         "message": "Short URL not found"
+//     })))
+// }
